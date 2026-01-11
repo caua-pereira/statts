5fbf31891d7c8e67260e6329aa9c221a0ce00c15
Cauã Pereira <caua.souza@ufv.br>
2026-01-11 18:33:31 -0300

    update da estrutura das médias

--- src/descriptive/mean.ts
+++ src/descriptive/mean.ts
@@ -5,17 +5,16 @@
 
 import { error } from "node:console";
 
+const verificaArrayVazio = (vetor: number[], operacao: string) => {
+    if (vetor.length === 0) 
+        throw new Error(`O cálculo da ${operacao} não está definido para conjuntos de dados vazios.`);    
+}
+
+
 export function mediaAritmetica(valores: number[]): number {
-    if(valores.length === 0) {
-        throw new Error('A Média Aritmética não está definida para conjutos de dados vazios');
-    }
-
-    let total: number = 0;
-    total = valores.reduce((acumulador, elemento) => {
-        return acumulador + elemento;
-    })
-
-    return total/valores.length;
+    verificaArrayVazio(valores, "média aritmetica" );
+    const somaDosValores: number = valores.reduce((acumulador, elemento) => {return acumulador + elemento}, 0);
+    return somaDosValores/valores.length;
 }
 
 /**
@@ -24,26 +23,17 @@ export function mediaAritmetica(valores: number[]): number {
  */
 
 export function mediaPonderada(valores: number[], pesos: number[]): number {
-    if(valores.length === 0) {
-        throw new Error('A Média Ponderada não está definida para conjutos de dados vazios');
-    }
+    verificaArrayVazio(valores, "média ponderada");
 
     if(valores.length !== pesos.length) {
-        throw new Error('A quatidade de pesos deve ser igual a quantidade de valores do conjunto');
+        throw new Error('A quatidade de pesos deve ser igual a quantidade de valores do conjunto.');
     }
 
-    let numerador: number;
-    numerador = valores.reduce((acumulador, elemento, indice) => {
-        return acumulador + elemento * pesos[indice];
-    }, 0);
+    const numerador: number = valores.reduce((acumulador, elemento, indice) => {return acumulador + elemento * pesos[indice]}, 0);
+    const denominador: number = pesos.reduce((acumulador, elemento) => {return acumulador + elemento}, 0);
 
-    let denominador: number = 0;
-    denominador = pesos.reduce((acumulador, elemento) => {
-        return acumulador + elemento;
-    });
-
-    if(denominador === 0) {
-        throw new Error('Soma dos pesos deve ser diferente de 0');
+    if(Math.abs(denominador) < Number.EPSILON) { // como EPSILON é o menor número possível entre 1 e o próximo número, daí: "se for muito, muito próximo de zero"
+        throw new Error('Deve haver pelo menos um peso não nulo no conjunto de dados.');
     }
 
     return numerador/denominador;
@@ -55,15 +45,15 @@ export function mediaPonderada(valores: number[], pesos: number[]): number {
  */
 
 export function mediaGeometrica(valores: number[]): number {
-    if(valores.length === 0) {
-        throw new Error('A Média Geométrica não está definida para conjuntos de dados vazios');
+    verificaArrayVazio(valores, "média geométrica");
+
+    const temValorNegativo: boolean = valores.some((valor) => {return valor <= 0});
+    if(temValorNegativo) {
+        throw new Error('A média geométrica não está definida para conjunto de elementos menores ou iguais a zero (0).');
     }
 
-    let produtoDosValores: number = valores.reduce((acumulador, elemento) => {
-        return acumulador * elemento;
-    }, 1);
-
-    return Math.sqrt(produtoDosValores);
+    const produtoDosValores: number = valores.reduce((acumulador, elemento) => {return acumulador * elemento}, 1);
+    return Math.pow(produtoDosValores, 1/valores.length);
 }
 
 /*
@@ -72,19 +62,16 @@ export function mediaGeometrica(valores: number[]): number {
  */
 
 export function mediaHarmonica(valores: number[]): number {
-    if(valores.length === 0) {
-        throw new Error('A Média Harmônica não está definida para conjuntos de dados vazios');
+    verificaArrayVazio(valores, "média harmônica");
+
+    const temValorNulo = valores.some((valor) => {return valor === 0});
+    if(temValorNulo) {
+        throw new Error('A média harmônica não está definida para conjunto de valores que possuam pelo menos um valor nulo (0).')
     }
 
-    valores.some(valor => {
-        if(valor === 0) {
-            throw new Error('Não pode haver valores nulos (0) na Média Harmônica');
-        }
-    })
-
-    let sumElementosInversos = valores.reduce((acumulador, elemento) => {
-        return acumulador + (1/elemento);
-    }) 
-
-    return valores.length/sumElementosInversos;
+    const somaElementosInversos = valores.reduce((acumulador, elemento) => {return acumulador + (1/elemento)}, 0); 
+    if (Math.abs(somaElementosInversos) < Number.EPSILON) {
+        throw new Error('A soma dos inversos é muito próxima de zero.');
+    }
+    return valores.length/somaElementosInversos;
 }
